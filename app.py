b0822900133e57a964639f9e66a138aa8555ecf2
kanikadatt <54899253+kanikadatt@users.noreply.github.com>
2019-11-05 23:04:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,23 +5,15 @@ Created on Tue Nov  5 20:24:32 2019
 @author: Kanika Chopra
 """
 
-from flask import render_template, Flask, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def home():
     return render_template('index.html')
 
-@app.route('/', methods = ['POST','GET'])
-def get_data():
-    if request.method == 'POST':
-        start = request.form.get('start')
-        end = request.form.get('end')
-        
-        insiderscore = insiderplot.return_json(start, end)
-        
-    return render_template('index.html', insider = insiderscore)
 
-if __name__=="__main__":
-    app.run(debug = False)
+if __name__ == '__main__':
+    app.run(debug=True)
